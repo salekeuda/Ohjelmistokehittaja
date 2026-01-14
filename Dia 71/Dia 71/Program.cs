4be12fa98fe200d748295a8c987bdfdaaece3937
Sami Leppänen <sami.leppanen@edu.keuda.fi>
2026-01-14 12:48:15 +0200

    dia71 myos dia72

--- Dia 71/Dia 71/Program.cs
+++ Dia 71/Dia 71/Program.cs
@@ -10,7 +10,12 @@
             //JakoJaannos();
             //NimiPyynto();
             //Kertotaulu();
-            ikakys();
+            //ikakys();
+            //luku();
+            //KirjainVaihto(); 
+            //PosiMi();
+            //PiLa();
+            Parittomat();
         }
 
         static void LaskeYhteen()
@@ -85,7 +90,7 @@
         {
             Console.WriteLine("Anna luku väliltä 1-10");
             int luku = int.Parse(Console.ReadLine());
-            for (int i = 1; i <= 10; i++)
+            for (int i = 1; i <= 10; i++)   // for silmukka
             {
                 Console.WriteLine(luku + " x " + i + " = " + (luku * i));
 
@@ -100,9 +105,91 @@
             a = Console.ReadLine();
             Console.WriteLine(ika(a) + "- näytät ikäistäsi nuoremmalta.");
         }
-        static string ika(string a)
+        static string ika(string a) // palauttaa merkkijonon
         {
-            return a;
+            return a; // palauttaa käyttäjän syöttämän iän
+        }
+
+        static void luku()
+        {
+            Console.Write("Anna luku (10 tai suurempi): ");
+            int luku = int.Parse(Console.ReadLine());
+
+            if (luku < 10)
+            {
+                Console.WriteLine("Luvun piti olla isompi kuin 10");
+                return; // Lopettaa metodin tähän
+            }
+            for (int i = 1; i < luku; i++) // i alkaa 1:stä ja jatkuu niin kauan kuin i on pienempi kuin luku
+            {
+                Console.Write(luku + " "); // "" ilman välilyöntiä tekee luvut yhteen
+            }
+        }
+        static void KirjainVaihto()
+        {
+              Console.Write("Anna sana: ");
+            string sana = Console.ReadLine();
+
+            if (sana.Length < 2) // tarkistaa sanan pituuden 
+            {
+                Console.WriteLine("Sanan täytyy olla vähintään kaksi kirjainta pitkä.");
+                return;
+            }   
+            char eka = sana[0];
+            char vika = sana[sana.Length - 1]; // viimeinen kirjain
+            string keskiosa = sana.Substring(1, sana.Length - 2); // keskiosa
+            string uusi = vika + keskiosa + eka;
+            Console.WriteLine("Uusi sana: " + uusi);
+        }
+        static void PosiMi()
+        {
+            Console.Write("Anna ensimmäinen luku: "); // pyytää käyttäjältä ensimmäisen luvun
+            int luku1 = int.Parse(Console.ReadLine()); //   muuntaa syötteen kokonaisluvuksi
+            Console.Write("Anna toinen luku: "); // pyytää käyttäjältä toisen luvun
+            int luku2 = int.Parse(Console.ReadLine()); // muuntaa syötteen kokonaisluvuksi
+            int tulos = luvut(luku1, luku2); // kutsutaan funktiota luvut
+            if (tulos > 0) // tarkistaa onko tulos positiivinen
+            {
+                Console.WriteLine("Luvut ovat positiivisia.");
+            }
+            else if (tulos < 0)// tarkistaa onko tulos negatiivinen
+            {
+                Console.WriteLine("Luvut ovat negatiivisia.");
+            }
+            else// tarkistaa onko toinen luvuista positiivinen ja toinen negatiivinen
+            {
+                Console.WriteLine("Toinen on positiivinen ja toinen on negatiivinen."); // tulos on nolla
+            }
+        }
+
+        static int luvut(int luku1, int luku2) // funktio joka palauttaa kahden luvun summan
+        {
+            return luku1 + luku2; // palauttaa lukujen summan
+        }
+
+        static void PiLa()
+        {
+            String str = "Tämä on esimerkki merkkijonosta.";
+           
+            string[] sana = str.Split(' '); // jakaa merkkijonon sanoiksi välilyönnin kohdalta
+            string pisin = ""; // alustaa pisin-muuttujan tyhjäksi merkkijonoksi
+            foreach (string s in sana) // käy läpi jokaisen sanan
+            {
+                if (s.Length > pisin.Length) // vertaa sanan pituutta pisin-muuttujan pituuteen
+                {
+                    pisin = s; // päivittää pisin-muuttujan, jos sana on pidempi
+                }
+            }
+            Console.WriteLine(pisin); // tulostaa pisimmän sanan
+        }
+        static void Parittomat()
+        {
+            for (int i = 1; i <= 99; i += 3) //  lisää 2 parittomat tai 3 jos joka kolmas
+            {
+                Console.WriteLine(i); // tulostaa parittoman luvun
+            }
         }
     }
 }
+
+
