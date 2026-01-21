bc43ccd9bfe422301340f528e4b505db339ff5bc
Sami Leppänen <sami.leppanen@edu.keuda.fi>
2026-01-21 09:53:38 +0200

    kesken

---
+++ LumoakatemiaIfElse/LumoakatemiaIfElse/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace LumoakatemiaIfElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SuurinLuku();
+        }
+        static void SuurinLuku()
+        {
+            Console.Write("Anna ensimmäinen kokonaisluku: ");
+            int eka = int.Parse(Console.ReadLine());
+            Console.Write("Anna toinen kokonaisluku: ");
+            int toka = int.Parse(Console.ReadLine());
+            if (eka > toka)
+            {
+                Console.WriteLine("{0} on suurempi kuin {1}", eka, toka);
+            }
+            else
+            {
+                Console.WriteLine("{0} on suurempi kuin {1}", toka, eka);
+            }
+          }
+        
+    }
+}
