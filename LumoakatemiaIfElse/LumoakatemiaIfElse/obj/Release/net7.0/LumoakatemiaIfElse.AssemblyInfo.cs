5236ad5ebdb48ba11470d7f749648c7b6cc79d9d
Sami Leppänen <sami.leppanen@edu.keuda.fi>
2026-02-04 10:54:28 +0200

    la valmis

--- LumoakatemiaIfElse/LumoakatemiaIfElse/obj/Release/net7.0/LumoakatemiaIfElse.AssemblyInfo.cs
+++ LumoakatemiaIfElse/LumoakatemiaIfElse/obj/Release/net7.0/LumoakatemiaIfElse.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("LumoakatemiaIfElse")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09ded3f5e48db11c54e2eeed6d981a910b6dab89")]
 [assembly: System.Reflection.AssemblyProductAttribute("LumoakatemiaIfElse")]
