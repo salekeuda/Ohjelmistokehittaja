6b7b255728e8b02a5a190c2656215743f7758817
Sami Leppänen <sami.leppanen@edu.keuda.fi>
2026-02-11 10:00:39 +0200

    t14ongelma

---
+++ Harjoitus14/Harjoitus14/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Harjoitus14
+{
+    public partial class paivakirjaFM : Form
+    {
+        public paivakirjaFM()
+        {
+            InitializeComponent();
+
+            // Lue vanha päiväkirja ja näytä se tekstilaatikossa
+            string teksti = File.ReadAllText("C:\\Temp\\paivakirja.txt");
+            kirjaTB.Text = teksti;
+        }
+
+        private void tallennaBT_Click(object sender, EventArgs e)
+        {
+            string teksti = "";
+            // Luetaan nykyinen sisältö
+            teksti += kirjaTB.Text;
+
+            // Lisätään uusi merkintä + aikaleima
+            teksti += "\n" + DateTime.Now.ToString("dd.MM.yyyy HH:mm") + "\n";
+
+            // Tallennetaan tiedostoon
+            TextWriter text = new StreamWriter("C:\\Temp\\paivakirja.txt");
+            text.Write(teksti);
+            text.Close();
+
+            // Suljetaan ohjelma
+            Application.Exit();
+        }
+    }
+}
+
